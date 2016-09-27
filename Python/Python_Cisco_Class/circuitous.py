96c8a721220d5f00297e415fe332b50134ff3d19
Pruthwin <kadmajeg@usc.edu>
2016-09-26 17:06:35 -0700

    Python Class Day 1

---
+++ Python/Python_Cisco_Class/circuitous.py
@@ -0,0 +1,71 @@
+''' Circuitous(tm)                         # Give the project a name
+An advanced circle analytic solution       # Elevator pitch -- what problem is being solved and how
+'''
+
+# Waterfall:  Requirements -> Design -> Coding -> Testing -> Documentation
+# Agile:      Do a little req->des->cod->test->doc and repeat frequently <-- learning process
+# Lean startup:  Create an MVP, ship it, get feedback, and alter course accordingly
+# Generalization / Specialization is a poor model for object oriented programming because of penguins, dophins, and bats
+# Inheritance: This is a tool whose sole purpose is code reuse.
+# In new-style classes, we inherit from object() which gives us a new __getattribute__
+# New-style classes have a much more interesting and powerful dot operator than old-style classes
+# We typically don't put docstrings on dunder methods because
+# 1) user's rarely see them and 2) the meaning of the dunder methods is well-known
+# When copying from one namespace to another we usually keep the variable name the same
+# the main exception is when the target namespace has its own jargon or ontology.
+# With magic constants, we factor them out to give them a name for improved intelligiblity
+# and express "a single source of truth"
+# The purpose of modules is to 1) organize code and 2) to support code reuse,
+# and 3) provide a single source of truth across an application.
+# MVP:  Minimum viable product
+# When creating an MVP, aim for the customer's biggest headache or for the crux of the problem
+# YAGNI,RN:  You ain't gonna need it, right now.
+# "Code is your enemy"
+# Dogfooding
+
+# Dunder methods:
+#   print s                  ->   s.__str__()
+#   >>> s                    ->   s.__repr__()
+#   s[0]                     ->   s.__getitem__(0)
+#   len(s)                   ->   s.__len__()
+#   for c in s:              ->   s.__iter__() until StopIteration
+#                            ->   s.__getitem__(0)  until IndexError
+#   s(a, b)                  ->   s.__call__(a, b)
+#   s.m                      ->   s.__getattribute__('m')
+#   s + t                    ->   s.__add__(t)
+
+import math
+from collections import namedtuple
+
+Version = namedtuple('Version', ['major', 'minor', 'micro'])   # Create a tuple with named fields
+
+class Circle(object):
+    'An advanced circle analytics toolkit for support circle analysis'
+
+    version = Version(0, 1, 1)          # Class variables store data that is SHARED by all instances and the class itself
+
+    def __init__(self, radius):
+        self.radius = radius            # Instance variables store data that is UNIQUE to each instance
+
+    def area(self):
+        'Perform quadrature on a planar shape of uniform revolution'
+        return math.pi * self.radius ** 2.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Python/Python_Cisco_Class/client_code.py
@@ -0,0 +1,17 @@
+"Show-off the ciruitous code from the user's point of view"
+
+from circuitous import Circle
+
+print u'Tutorial for Circuitous\N{trade mark sign}'
+print 'Circle version %d.%d' % Circle.version[:2]
+c = Circle(10)
+print 'A circle with a radius of', c.radius
+print 'has an area of', c.area()
+print
+
+## Academic Friends ######################################
+
+print 'DARPA Grant Proposal'
+print 'to study the average area of random circles'
+print 'using Circuitous(tm) version %d.%d' % Circle.version[:2]
+
